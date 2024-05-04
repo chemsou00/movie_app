6e66708bb4fb8d3b09a42bf78f43440a1eaabac7
chemsou00 <souchem00@gmail.com>
2024-05-04 01:34:51 +0100

    change name and IconApp

--- lib/core/resources/app_strings.dart
+++ lib/core/resources/app_strings.dart
@@ -1,5 +1,5 @@
 class AppStrings {
-  static const String appTitle = 'Movies App';
+  static const String appTitle = 'MoviesDoc';
   static const String seeAll = 'see all';
   static const String popularMovies = 'Popular movies';
   static const String topRatedMovies = 'Top rated movies';
